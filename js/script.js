a087901f2a4a3f534e8990bb55e7e4dbaf0f51f0
Mba Thomas <50582051+FabulousThomas@users.noreply.github.com>
2020-12-11 04:56:58 +0100

    Updated:

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var div = document.createElement('div');
   div.id = 'container';
-  div.innerHTML = 'Hi there!';
- // div.className = 'border pad';
+  div.innerHTML += editor1;
+  // div.className = 'border pad';
 
   document.body.appendChild(div);
 }, false);
