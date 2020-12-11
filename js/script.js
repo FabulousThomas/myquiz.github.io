2640c4e778419aafe682aff497ca116c644c1b07
Mba Thomas <50582051+FabulousThomas@users.noreply.github.com>
2020-12-11 04:42:13 +0100

    Added: js/script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var div = document.createElement('div');
+  div.id = 'container';
+  div.innerHTML = 'Hi there!';
+ // div.className = 'border pad';
+
+  document.body.appendChild(div);
+}, false);
